ef4eb4cafe48455183e13812d84beede87b446b5
KUSHL <KUSHL>
2024-01-31 00:11:45 +0530

    new method to solve count grid paths

--- GENERAL/013_Count_Paths.py
+++ GENERAL/013_Count_Paths.py
@@ -27,23 +27,20 @@ class Solution:
 dp solution
 """
 class Solution:
-    def countPaths(self, i: int, j: int, n: int, m: int, dp: List[List[int]]) -> int:
-        if i == (n - 1) and j == (m - 1):
-            return 1
-        if i >= n or j >= m:
-            return 0
-        if dp[i][j] != -1:
-            return dp[i][j]
-        else:
-            dp[i][j] = self.countPaths(
-                i + 1, j, n, m, dp) + self.countPaths(i, j + 1, n, m, dp)
-            return dp[i][j]
+    def countPaths(self,
+                   i: int,
+                   j: int,
+                   n: int,
+                   m: int) -> int :
+        dp = [0 for _ in range(n) for __ in range(m)]
 
+        for _ in range(m) :
+            dp[_][0] = 1
+        for _ in range(n) :
+            do[0][_] = 1
 
-    def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[-1 for _ in range(n + 1)] for _ in range(m + 1)]
+        for _ in range(1, m) :
+            for __ in range(1, n) :
+                dp[_][__] = dp[_-1][__] + dp[_][__-1]
 
-        num = self.countPaths(0, 0, m, n, dp)
-        if m == 1 and n == 1:
-            return num
-        return dp[0][0]
+        return dp[m-1][n-1]
